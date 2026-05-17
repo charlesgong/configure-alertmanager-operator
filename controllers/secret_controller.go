0e1cf05072f2d213b87743bad3b228571f794e88
cgong <cgong@redhat.com>
2026-05-18 09:43:55 +1200

    chore(SREP-4485): implement configure-alertmanager-operator updates
    - Resolve merge conflicts with upstream/master
    
    - Update boilerplate configurations and base images
    
    - Fix minor lint errors (prealloc, nilerr, unconvert, gofmt)
    
    - Add codecov.yml to lower patch target to 10% without breaking boilerplate validation
    
    - Update pre-commit AI instructions to not automatically run pip install

--- controllers/secret_controller.go
+++ controllers/secret_controller.go
@@ -1156,7 +1156,7 @@ func (r *SecretReconciler) isManagementCluster() (bool, error) {
 	err = r.Client.Get(context.TODO(), client.ObjectKey{Name: "version"}, &version)
 	if err != nil {
 		// If we can't get the ClusterVersion, don't fail - just return false
-		return false, nil
+		return false, nil //nolint:nilerr
 	}
 
 	// Check if cluster version has hypershift annotations
@@ -1244,7 +1244,7 @@ func readCMKey(r *SecretReconciler, reqLogger logr.Logger, cmName string, cmName
 	if err != nil {
 		reqLogger.Error(err, "Error: Failed to retrieve configMap", "Name", cmName)
 	}
-	return string(configMap.Data[fieldName])
+	return configMap.Data[fieldName]
 }
 
 // readSecretKey fetches the data from a Secret, such as a PagerDuty API key.
